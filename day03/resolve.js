45b944168836c07707ea4530184d309479757b3b
lucyhebert <lucyhebert@free.fr>
2020-12-03 15:58:53 +0100

    Solve day 3 part 2

--- day03/resolve.js
+++ day03/resolve.js
@@ -6,11 +6,50 @@ function solveDay5(filePath) {
 
 	const lines = fs.readFileSync(filePath,'utf8').split('\n')
 
-	console.log(countEncounteredTrees(lines))
+	const slopes = [
+		{
+			right: 1,
+			down: 1
+		},
+		{
+			right: 3,
+			down: 1
+		},
+		{
+			right: 5,
+			down: 1
+		},
+		{
+			right: 7,
+			down: 1
+		},
+		{
+			right: 1,
+			down: 2
+		}
+	]
+
+	console.log(countEncounteredTrees(lines, slopes[1]))
+	console.log(multiplySlopesTreesCounters(lines, slopes))
 
 }
 
-function countEncounteredTrees(lines) {
+function multiplySlopesTreesCounters(lines, slopes) {
+	const slopesLength = slopes.length
+	let result = 1
+
+	let i = 0
+
+	while(i < slopesLength) {
+		let treesCount = countEncounteredTrees(lines, slopes[i])
+		result = result * treesCount
+		i++
+	}
+
+	return result
+}
+
+function countEncounteredTrees(lines, slope) {
 	const linesLength = lines.length //1000
 	const lineWidth = lines[0].length //17
 
@@ -28,8 +67,8 @@ function countEncounteredTrees(lines) {
 			treesCounter++
 		}
 
-		yCursor = (yCursor + 3) % lineWidth //3 to right
-		i++ //1 to bottom
+		yCursor = (yCursor + slope.right) % lineWidth
+		i += slope.down
 
 
 	}
