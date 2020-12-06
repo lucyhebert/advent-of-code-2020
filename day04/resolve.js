42ac04193464092a222f759e0c91e1435519be14
lucyhebert <lucyhebert@free.fr>
2020-12-06 16:54:52 +0100

    Solve day 4 part 2

--- day04/resolve.js
+++ day04/resolve.js
@@ -1,47 +1,68 @@
 const fs = require('fs')
 
-solveDay5('day04/input.txt')
+solveDay4('day04/input.txt')
 
-function solveDay5(filePath) {
+function solveDay4(filePath) {
 
 	const lines = fs.readFileSync(filePath,'utf8')
 
-	const regexp = /(\n{1})([a-z])/gm
-	const passportsPart1 = lines.replace(regexp, " $2").split('\n')
+	const passports = lines.replace(/(\n{1})(.)/gm, " $2").split('\n')
 
-	console.log(countValidPassportsPart1(passportsPart1))
+	console.log(countValidPassports(passports))
 }
 
-function isValidPassportPart1(passport, requiredFields) {
-	return requiredFields.every(el => passport.toLowerCase().includes(el.toLowerCase()));
-}
+function countValidPassports(passports) {
 
-function countValidPassportsPart1(passportsPart1) {
+	const passportsLength = passports.length
 
-	const passportsLength = passportsPart1.length
-	const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-
-	let passport
 	let validPassportsCounter = 0
+	let validPassportPart2Counter = 0
 
 	let i = 0
 
-	while(i < passportsLength) {
-		passport = passportsPart1[i].trim()
+	while (i < passportsLength) {
+		let passport = passports[i]
 
-		if(isValidPassportPart1(passport, requiredFields)) {
-			validPassportsCounter ++
+		if(isValidPassportPart1(passport)) {
+			validPassportsCounter++
+
+			if(isValidPassportPart2(passport)) {
+				validPassportPart2Counter++
+			}
+		}
+
+		i++
+	}
+
+	return [validPassportsCounter, validPassportPart2Counter]
+}
+
+function isValidPassportPart1(passport) {
+	const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+	return requiredFields.every(el => passport.toLowerCase().includes(el.toLowerCase()));
+}
+
+function isValidPassportPart2(passport) {
+
+	const regexpList = [
+		/byr:(19[2-9][0-9]|200[0-2])/g,
+		/iyr:(201[0-9]|2020)/g,
+		/eyr:(202[0-9]|2030)/g,
+		/hgt:(((1[5-8][0-9])|(19[0-3]))cm|(59|(6[0-9])|(7[0-6]))in)/g,
+		/hcl:#([0-9]|[a-f]){6}/g,
+		/ecl:(amb|blu|brn|gry|grn|hzl|oth)/g,
+		/pid:\b[0-9]{9}\b/g
+	]
+
+	let regexpsListLength = regexpList.length
+
+	let i = 0
+	while(i < regexpsListLength) {
+		if(!passport.match(regexpList[i])) {
+			return false
 		}
 		i++
 	}
 
-	return validPassportsCounter
-}
-
-function isValidPasswordPart1(password, letter, min, max) {
-	let letterRegexp = new RegExp(letter, 'g')
-
-	const letterMatchesLength = password.match(letterRegexp).length
-
-	return letterMatchesLength >= min && letterMatchesLength <= max
+	return true
 }
