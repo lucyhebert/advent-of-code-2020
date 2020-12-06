94de80d8a8c2975415db1cd6147454fb64441e4b
lucyhebert <lucyhebert@free.fr>
2020-12-06 22:11:27 +0100

    Solve day 6 part 2

--- day06/resolve.js
+++ day06/resolve.js
@@ -4,7 +4,7 @@ solveDay6('day06/input.txt')
 
 function solveDay6(filePath) {
 
-	const groups = fs.readFileSync(filePath,'utf8').replace(/(\n{1})(.)/gm, "$2").split('\n')
+	const groups = fs.readFileSync(filePath,'utf8').replace(/(\n{1})(.)/gm, " $2").split('\n')
 
 	console.log(countTotalPositiveAnswers(groups))
 }
@@ -14,33 +14,46 @@ function countTotalPositiveAnswers(groups) {
 	const groupsLength = groups.length
 
 	let totalPositiveAnswers = 0
+	let totalPositiveAnswersForEveryoneInGroup = 0
 	let i = 0
 
 	while (i < groupsLength) {
 		let group = groups[i]
 
-		totalPositiveAnswers += countPositiveAnswersForGroup(group)
+		let result = countPositiveAnswersInGroup(group)
+		totalPositiveAnswers += result[0]
+		totalPositiveAnswersForEveryoneInGroup += result[1]
 
 		i++
 	}
 
-	return totalPositiveAnswers
+	return [totalPositiveAnswers, totalPositiveAnswersForEveryoneInGroup]
 }
 
-function countPositiveAnswersForGroup(group) {
-	const groupLength = group.length
 
-	let positiveAnswers = []
-	let i =0
+function countPositiveAnswersInGroup(group) {
+	const groupLength = group.trim().split(' ').length
+	const sortedGroupAnswers = (group.replace(/\s/g, '')).split('').sort().join('')
 
-	while(i < groupLength) {
-		let positiveAnswer = group[i]
-		if(!positiveAnswers.includes(positiveAnswer)) {
-			positiveAnswers.push(positiveAnswer)
+	let positiveAnswersCounter = 0
+	let positiveAnswersForEveryoneCounter = 0
+
+	let i = 0
+
+	while(i < sortedGroupAnswers.length) {
+		if(i == 0 || i > 0 && sortedGroupAnswers[i] !== sortedGroupAnswers[i - 1]) {
+			let regexp = new RegExp(sortedGroupAnswers[i], 'g')
+			let answerCount = sortedGroupAnswers.match(regexp)?.length
+
+			positiveAnswersCounter++
+
+			if(answerCount === groupLength) {
+				positiveAnswersForEveryoneCounter++
+			}
 		}
 
 		i++
 	}
 
-	return positiveAnswers.length
+	return [positiveAnswersCounter, positiveAnswersForEveryoneCounter]
 }
